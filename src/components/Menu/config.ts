1e8ddfbcefdbc3276067677b44daec9ce7c18a15
mobile-dev-hybrid <cutecate2019118@gmail.com>
2021-05-20 11:52:56 +0300

    initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,134 @@
+import { MenuEntry } from '@soy-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  // {
+  //   label: 'Prediction',
+  //   icon: 'PredictionsIcon',
+  //   href: '/prediction',
+  //   status: {
+  //     text: 'BETA',
+  //     color: 'warning',
+  //   },
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'Collectibles',
+  //   icon: 'NftIcon',
+  //   href: '/collectibles',
+  // },
+  // {
+  //   label: 'Team Battle',
+  //   icon: 'TeamBattleIcon',
+  //   href: '/competition',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: '/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: '/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: '/profile',
+  //     },
+  //   ],
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: '#',
+      },
+      {
+        label: 'Tokens',
+        href: '#',
+      },
+      {
+        label: 'Pairs',
+        href: '#',
+      },
+      {
+        label: 'Accounts',
+        href: '#',
+      },
+    ],
+  },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: '/ifo',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Contact',
+        href: '#',
+      },
+      {
+        label: 'Voting',
+        href: '#',
+      },
+      {
+        label: 'Github',
+        href: '#',
+      },
+      {
+        label: 'Docs',
+        href: '#',
+      },
+      {
+        label: 'Blog',
+        href: '#',
+      },
+      {
+        label: 'Merch',
+        href: '#',
+      },
+    ],
+  },
+]
+
+export default config
