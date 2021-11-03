bdaad103515d5bcd652c97b3fae5b8ebd91b4de7
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-11-02 21:26:12 -0700

    updated bidder section

--- src/views/IDO/hooks/useGetUserDetail.ts
+++ src/views/IDO/hooks/useGetUserDetail.ts
@@ -13,7 +13,7 @@ const useGetUserDetail = () => {
     useEffect(() => {
         const getData = async () => {
             const contract = getDailyIdoContractWithAccount(library, account)
-            const res = await contract.getUserDetail('0xC7d98c4c919E93eD44755718E27b53791E7F3521')
+            const res = await contract.getUserDetail(account)
             const { lockedDate, lockedSoy, soyPrice} = res
             const soyLocked = parseFloat(res.soyLocked.toString()) / 1000000000000000000
             const soyToClaim = parseFloat(res.soyToClaim.toString()) / 1000000000000000000
