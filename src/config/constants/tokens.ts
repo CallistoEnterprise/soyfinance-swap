32dcecabaee78f5f776dd6d6e034e510ecefb507
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-01-06 08:01:39 -0800

    added xms token

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -18,17 +18,8 @@ export const SOY: { [chainId: number]: Token } = {
 }
 
 export const WCLO = new Token(ChainId.MAINNET, '0xF5AD6F6EDeC824C7fD54A66d241a227F6503aD3a', 18, 'WCLO', 'Wrapped CLO')
-// export const DAI = new Token(ChainId.MAINNET, '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', 18, 'DAI', 'Dai Stablecoin')
 export const BUSDT = new Token(ChainId.MAINNET, '0xbf6c50889d3a620eb42C0F188b65aDe90De958c4', 18, 'BUSDT', 'Tether USD')
-// export const BTCB = new Token(ChainId.MAINNET, '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6', 8, 'WBTC ', 'Wrapped BTC')
 
-// export const UST = new Token(
-//   ChainId.MAINNET,
-//   '0x692597b009d13C4049a947CAB2239b7d6517875F',
-//   18,
-//   'UST',
-//   'Wrapped UST Token',
-// )
 export const ETH = new Token(
   ChainId.MAINNET,
   '0xcC00860947035a26Ffe24EcB1301ffAd3a89f910',
@@ -36,13 +27,6 @@ export const ETH = new Token(
   'ccETH',
   'Wrapped Ether',
 )
-// export const USDC = new Token(
-//   ChainId.MAINNET,
-//   '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', // '0xc950a687e8d08e70fc072ca8c3596b62aef91faf',
-//   18,
-//   'USDC',
-//   'Polygon-Peg USD Coin',
-// )
 
 const tokens = {
   clo: {
