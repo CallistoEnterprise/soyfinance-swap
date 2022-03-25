24ca9504524f0b389ef8dbbec713105be78f2a99
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-03-25 10:06:47 -0700

    added learn link

--- src/views/Collectibles/index.tsx
+++ src/views/Collectibles/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Heading, Text } from '@soy-libs/uikit2'
+import { Heading, Text, LinkExternal } from '@soy-libs/uikit2'
 import { useTranslation } from 'contexts/Localization'
 import Page from 'components/Layout/Page'
 import NftList from './components/NftList'
@@ -24,6 +24,8 @@ const Collectibles = () => {
       <Text>The SOY Finance Charity NFT is designed as a reminder that we all share one planet, and we all need to spread one love.</Text>
       <Text mt="8px">Considering the charitable nature of the series, a minimum price is set, but no maximum price: everyone is free to donate as much as they wish.</Text>
       <Text mt="8px">All funds collected will be used to support people in need.</Text>
+
+      <LinkExternal href="https://soy-finance.gitbook.io/soy-finance/soy-products/nfts/one-earth-one-heart" marginTop="20px">Learn more about Charity NFTs</LinkExternal>
       <NftList />
     </Page>
   )
